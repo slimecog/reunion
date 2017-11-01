8e715f8d30cce5ccd06ff72288743695db721234
slimecog <wlcjohnson@gmail.com>
2017-11-01 10:59:31 -0600

    adds exercise

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,30 @@
 class Activity
+  attr_reader :name,
+              :participant,
+              :amount_paid,
+              :participants,
+              :total_cost
+
+  def initialize(name)
+    @name         = name
+    @participant  = participant
+    @amount_paid  = amount_paid
+    @participants = {}
+    @total_cost   = total_cost
+  end
+
+  def add_participant({participant: amount_paid})
+    if @participants.nil?
+      @participants = {participant: amount_paid}
+    else @participants.merge participants: amount_paid
+    end
+  end
+
+  def find_total_cost
+    @total_cost = participants.map {|cost| cost[participant]}.reduce(0, :+)
+  end
+
+  def split_total_cost
+    @total_cost / (@participants.count)
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,34 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_activity_has_a_name
+    activity = Activity.new("Hiking")
+
+    assert_equal "Hiking", activity.name
+  end
+
+  def test_activity_has_participants
+    activity = Activity.new("Hiking")
+    activity.add_participant("Gary", 10)
+
+    assert_equal "Gary", activity.participants.first.key
+    assert_equal 10, activity.participants.first.value
+  end
+  
+  def test_total_cost_can_be_found
+    activity = Activity.new("Hiking")
+    activity.add_participant("Gary", 10)
+    activity.add_participant("Susan", 10)
+
+    assert_equal 20, activity.find_total_cost
+  end
+
+  def test_total_cost_can_be_split_equally
+    activity = Activity.new("Hiking")
+    activity.add_participant("Gary", 10)
+    activity.add_participant("Susan", 10)
+
+    assert_equal 10, activity.split_total_cost
+  end
 end
